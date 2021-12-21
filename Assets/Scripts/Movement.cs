d344ef5267cc761bae6ab6b4fcd16cc19d9f3649
Giovanni Misitano <giovanni.misitano@jyu.fi>
2021-12-21 23:03:39 +0200

    Added particle effects for boosting and turning. Refactored Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,9 @@ public class Movement : MonoBehaviour
     [SerializeField] float thrust = 80.0f;
     [SerializeField] float rotationSpeed = 80.0f;
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem mainThrustParticle;
+    [SerializeField] ParticleSystem leftThrustParticle;
+    [SerializeField] ParticleSystem rightThrustParticle;
 
     Rigidbody rb;
     AudioSource audioSource;
@@ -29,36 +32,75 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Time.deltaTime * thrust * Vector3.up);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-            }
+            StartThrusting();
         }
         else
         {
-            if (audioSource.isPlaying)
-            {
-                audioSource.Stop();
-            }
+            StopThrusting();
         }
 
+    }
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainThrustParticle.Stop();
+    }
+
+    private void StartThrusting()
+    {
+        rb.AddRelativeForce(Time.deltaTime * thrust * Vector3.up);
+        mainThrustParticle.Play();
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
     }
 
     void ProcessRotation()
     {
+        // turning left
         if (Input.GetKey(KeyCode.A))
         {
-            ApplyRotation(rotationSpeed);
+            RotateLeft();
         }
 
+        // turning right
         else if (Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(-rotationSpeed);
+            RotateRight();
+        }
+        else
+        {
+            StopRotating();
         }
     }
 
-    private void ApplyRotation(float rotationThisFrame)
+    void StopRotating()
+    {
+        rightThrustParticle.Stop();
+        leftThrustParticle.Stop();
+    }
+
+    void RotateRight()
+    {
+        ApplyRotation(-rotationSpeed);
+        if (!leftThrustParticle.isPlaying)
+        {
+            leftThrustParticle.Play();
+        }
+    }
+
+    void RotateLeft()
+    {
+        ApplyRotation(rotationSpeed);
+        if (!rightThrustParticle.isPlaying)
+        {
+            rightThrustParticle.Play();
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
     {
         rb.freezeRotation = true; // freezing rotation so that we can manually rotate
         transform.Rotate(Time.deltaTime * rotationThisFrame * Vector3.forward);
